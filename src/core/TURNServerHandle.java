b70ebe31cec627da0f3950ce221c0c3edd4b7f8c
yfruan <1fanruan@gmail.com>
2015-01-09 11:14:04 +0100

    debug

--- src/core/TURNServerHandle.java
+++ src/core/TURNServerHandle.java
@@ -29,11 +29,10 @@ public class TURNServerHandle extends ServerHandle{
 		Endpoint publicEndpoint=new Endpoint(remoteAddress,remotePort);
 		
 		
-    	//Message message=(Message) SerializationUtils.deserialize(receivedPacket.getData());
+    	Message message=(Message) SerializationUtils.deserialize(receivedPacket.getData());
     	    	
 		if(relayEndpoints.containsKey(publicEndpoint)){		
 			try{
-				/*
 				if(message.getEventHeader()!=EventHeader.PING){
 			
 					Endpoint otherEndpoint=relayEndpoints.get(publicEndpoint);
@@ -47,11 +46,11 @@ public class TURNServerHandle extends ServerHandle{
 
 					server.sendMessage(message, otherEndpoint.getAddress(), otherEndpoint.getPort());
 					//UDPServer.sendMessage(receivedPacket.getData(), publicEndpoint.getAddress(), publicEndpoint.getPort());
-				}*/
+				}
 				
-				Endpoint otherEndpoint=relayEndpoints.get(publicEndpoint);
-				System.out.println("Relay message from "+publicEndpoint+" to "+otherEndpoint);
-				server.sendMessage(receivedPacket.getData(), otherEndpoint.getAddress(), otherEndpoint.getPort());
+				//Endpoint otherEndpoint=relayEndpoints.get(publicEndpoint);
+				//System.out.println("Relay message from "+publicEndpoint+" to "+otherEndpoint);
+				//server.sendMessage(receivedPacket.getData(), otherEndpoint.getAddress(), otherEndpoint.getPort());
 			}
 			catch(Exception e){
 				e.printStackTrace();
